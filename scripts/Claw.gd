69cb440783a7bb0936cec77e3000f3e7a955f445
Vkiti1 <viktor.skific@gmail.com>
2020-12-09 23:23:54 +0100

    pucanje, collision detection, score, ninja

--- scripts/Claw.gd
+++ scripts/Claw.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
+signal pickedUpHuman
 
-export (int) var speed = 400
+export (int) var speed = 4
 export (int) var shootingSpeed = 5
 
+var score = 0
 var velocity = Vector2()
 var hitFloor = false
 var touchingLeftWall = false
@@ -11,7 +13,7 @@ var shooting = false
 
 func get_input():
 	velocity = Vector2()
-	#dok se vraca gore ne mici po x i vracaj po y(i dalje mozes micat po x iz nekog razloga)
+	#dok se vraca gore ne mici po x i vracaj po y
 	if hitFloor:
 		velocity.x = 0
 		velocity.y -= 1
@@ -19,15 +21,28 @@ func get_input():
 	elif shooting:
 		velocity.x = 0
 		velocity.y += 1
-	elif Input.is_action_pressed('ui_right') and not touchingRightWall:
-		velocity.x += 1
 	elif Input.is_action_pressed('ui_left') and not touchingLeftWall:
 		velocity.x -= 1
+	elif Input.is_action_pressed('ui_right') and not touchingRightWall:
+		velocity.x += 1
+	
 
+func _ready():
+	score = 0
 
 func _input(ev):
 	if ev.is_action_pressed('ui_accept'):
 		shooting = true
+	   
+
+
+func checkCollision(collision):
+	if(collision):
+		var inst = instance_from_id(collision.get_collider_id())
+		if inst is KinematicBody2D:
+			score += inst.pointsToGive
+			inst.queue_free()
+			emit_signal("pickedUpHuman")
 
 
 func _physics_process(_delta):
@@ -36,26 +51,27 @@ func _physics_process(_delta):
 		velocity *= speed * shootingSpeed
 	else:	
 		velocity *= speed
-	velocity = move_and_slide(velocity)
+	var collision = move_and_collide(velocity)
+	checkCollision(collision)
 
 
 func _on_WallLeft_body_entered(body):
-	if(body.get_instance_id() == 1299):
+	if(body.get_instance_id() == get_instance_id()):
 		touchingLeftWall = true
 
 
 func _on_WallRight_body_entered(body):
-	if(body.get_instance_id() == 1299):
+	if(body.get_instance_id() == get_instance_id()):
 		touchingRightWall = true
 
 
 func _on_WallLeft_body_exited(body):
-	if(body.get_instance_id() == 1299):
+	if(body.get_instance_id() == get_instance_id()):
 		touchingLeftWall = false
 
 
 func _on_WallRight_body_exited(body):
-	if(body.get_instance_id() == 1299):
+	if(body.get_instance_id() == get_instance_id()):
 		touchingRightWall = false
 
 
